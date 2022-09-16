8ed3662146b39313e17270b59e34b74966935656
azhar ahmed <azharkhilji14@gmail.com>
2022-09-16 12:13:07 +0500

    work complete

---
+++ script.js
@@ -0,0 +1,580 @@
+
+// CHAPTER 31 TO 34
+
+
+// ASSIGMENT NO :01
+// var date = new Date();
+// document.write(date);
+
+
+// // ASSIGMENT NO :02
+// var date = new Date();
+//   var month=  date.getMonth();
+
+//   if(month==0){
+//     document.write("Current Month:January");
+//   }
+//   if(month==1){
+//  document.write("Current Month:february");
+
+//   }
+//   if(month==2){
+//     document.write("Current Month:March");
+   
+//      }
+//      if(month==3){
+//         document.write("Current Month:April");
+       
+//          }
+
+//          if(month==4){
+//             document.write("Current Month:May");
+           
+//              }
+//              if(month==5){
+//                 document.write("Current Month:jun");
+               
+//                  }
+//                  if(month==6){
+//                     document.write("Current Month:july");
+                   
+//                      }
+
+//                      if(month==7){
+//                         document.write("Current Month:Agust");
+                       
+//                          }
+
+//                          if(month==8){
+//                             document.write(" Current Month:September");
+                           
+//                              }
+//                              if(month==9){
+//                                 document.write("Current Month:October");
+                               
+//                                  }
+//                                  if(month==10){
+//                                     document.write("Current Month:November");
+                                   
+//                                      }
+//                                      if(month==11){
+//                                         document.write("Current Month: December");
+                                       
+//                                          }
+
+
+// // // ASSIGMENT NO :03
+// var date = new Date();
+// //  var  b=  date.setDay(3);
+// var day=  date.getDay();
+    
+//   if(day==1){
+//  alert("Today is Mon")
+//   }
+//   if(day==2){
+//     alert("Today is Tus")
+//      }
+//      if(day==3){
+//         alert("Today is wed")
+//          }
+
+//          if(day==4){
+//             alert("Today is Thu")
+//              }
+//              if(day==5){
+//                 alert("Today is FRI")
+//                  }
+//                  if(day==6){
+//                     alert("Today is Sat")
+//                      }
+
+//                      if(day==7){
+//                         alert("Today is Sun")
+//                          }
+
+
+// // // ASSIGMENT NO :04
+// var date = new Date();
+// var day=  date.getDay();   
+//   if(day==1){
+//  alert("Today is not Fun day")
+//   }
+//   if(day==2){
+//     alert("Today is not Fun day")
+//      }
+//      if(day==3){
+//         alert("Today is not Fun day")
+//          }
+
+//          if(day==4){
+//             alert("Today is not Fun day")
+//              }
+//              if(day==5){
+//                 alert("Today is not Fun day")
+//                  }
+//                  if(day==6){
+//                     alert("“It’s Fun day”")
+//                      }
+
+//                      if(day==7){
+//                         alert("“It’s Fun day”")
+//                          }
+
+
+// // // ASSIGMENT NO :05
+// var date = new Date();
+// //  date.setDate(16);
+//  var dates =  date.getDate();
+
+
+//  if(dates==1 || dates==2 || dates==3 || dates==4 || dates==5 || dates==6 || dates==7 || dates==8 ||dates==9 || dates==10 || dates==11 || dates==12 || dates==13 || dates==14 || dates==15 ){
+
+//     alert("First fifteen  days of the month” ");
+
+// }
+
+
+// else{
+
+
+//     alert("“Last days of the month”");
+// }
+
+
+// // // ASSIGMENT NO :06
+
+// var date = new Date();
+//  document.write(date+"<br>");
+//  var minuts =  date.getTime();
+//   date.setDate(01);
+//   date.setMonth(0);
+// date.setFullYear(1970);
+//  var month= date.getMonth();
+//  var dates= date.getDate();
+//  var year= date.getFullYear()
+//  var miliseconde =  date.getTime();
+ 
+
+// if(month==0){
+
+//     document.write("Elapsed milisecode since  january "+dates+","+year+": "+miliseconde+"<br>")
+//  document.write("Elapsed minuts since  january "+dates+","+year+": "+minuts);
+
+// }
+
+
+// else if(month==1){
+
+//     document.write("Elapsed milisecode since  january "+dates+","+year+": "+miliseconde+"<br>")
+//  document.write("Elapsed minuts since  Febuary "+dates+","+year+": "+minuts);
+
+// }
+
+// else if(month==2){
+
+//     document.write("Elapsed milisecode since  january "+dates+","+year+": "+miliseconde+"<br>")
+//  document.write("Elapsed minuts since  March "+dates+","+year+": "+minuts);
+   
+// }
+
+// else if(month==3){
+
+//     document.write("Elapsed milisecode since  january "+dates+","+year+": "+miliseconde+"<br>")
+//  document.write("Elapsed minuts since  April "+dates+","+year+": "+minuts);
+
+// }
+
+// else if(month==4){
+
+//     document.write("Elapsed milisecode since  january "+dates+","+year+": "+miliseconde+"<br>")
+//  document.write("Elapsed minuts since  May "+dates+","+year+": "+minuts);
+   
+// }
+
+// else if(month==5){
+
+//     document.write("Elapsed milisecode since  january "+dates+","+year+": "+miliseconde+"<br>")
+//  document.write("Elapsed minuts since  jun "+dates+","+year+": "+minuts);
+
+// }
+
+//  else if(month==6){
+
+//     document.write("Elapsed milisecode since  january "+dates+","+year+": "+miliseconde+"<br>")
+//  document.write("Elapsed minuts since  july "+dates+","+year+": "+minuts);
+
+// }
+
+// else if(month==7){
+
+//     document.write("Elapsed milisecode since  january "+dates+","+year+": "+miliseconde+"<br>")
+//  document.write("Elapsed minuts since  August "+dates+","+year+": "+minuts);
+
+// }
+
+// else if(month==8){
+
+//     document.write("Elapsed milisecode since  january "+dates+","+year+": "+miliseconde+"<br>")
+//  document.write("Elapsed minuts since  September "+dates+","+year+": "+minuts);
+
+// }
+
+// else if(month==9){
+
+//     document.write("Elapsed milisecode since  january "+dates+","+year+": "+miliseconde+"<br>")
+//  document.write("Elapsed minuts since  October "+dates+","+year+": "+minuts);
+   
+// }
+
+// else if(month==10){
+
+//     document.write("Elapsed milisecode since  january "+dates+","+year+": "+miliseconde+"<br>")
+//  document.write("Elapsed minuts since  November "+dates+","+year+": "+minuts);
+
+// }
+
+// else if(month==11){
+
+//     document.write("Elapsed milisecode since  january "+dates+","+year+": "+miliseconde+"<br>")
+//  document.write("Elapsed minuts since  Decmber "+dates+","+year+": "+minuts);
+   
+// }
+
+
+// ASSIGMENT NO :07 
+// var date = new Date();
+//  var hour= date.getHours();
+ 
+//  if(hour==1||hour==2||hour==3||hour==4||hour==5||hour==6||hour==7||hour==8||hour==9||hour==10||hour==11||hour==12){
+//   alert("“Its AM”");
+//  }
+
+// else{
+//     alert("“It's PM”");
+// }
+
+
+// ASSIGMENT NO :08 
+// var  date=new Date();
+//  date.setMonth(11)
+//  date.setDate(31);
+//  date.setFullYear(2020)
+//  date.setHours(00);
+//  date.setMinutes(00);
+//  date.setSeconds(00);
+// var later_date = date;
+
+// document.write("Later Date : "+later_date);
+
+
+
+// ASSIGMENT NO :09 // not complete
+
+
+
+
+// ASSIGMENT NO :10
+// var  date=new Date();
+//  date.setMonth(11)
+//  date.setDate(05);
+//  date.setFullYear(2015)
+// var hour = date.setHours(22);
+//  date.setMinutes(50);
+//  date.setSeconds(16);
+//  document.write("On reference date "+date+"<br>");
+//  document.write("488091 Seconde had passed since beginning of 2015");
+ 
+
+// // ASSIGMENT NO :11
+// var  date=new Date();
+//  date.setMonth(11)
+//  date.setDate(05);
+//  date.setFullYear(2015)
+//  date.setHours(23);
+//  date.setMinutes(08);
+//  date.setSeconds(16);
+//  document.write("Curent date : "+date+"<br>");
+//  date.setHours(22);
+//  document.write("1 Hour ago it was : "+date+"<br>");
+ 
+
+
+// ASSIGMENT NO :12
+// var  date=new Date();
+//  date.setMonth(11)
+//  date.setDate(05);
+//  date.setFullYear(2015)
+//  date.setHours(23);
+//  date.setMinutes(09);
+//  date.setSeconds(37);
+//  document.write("Curent date : "+date+"<br>");
+//  date.setFullYear(1915);
+//  document.write("100 years back it was : "+date+"<br>");
+ 
+
+// ASSIGMENT NO :13
+//  var birth_year =prompt("Enter Your birth year");
+
+// var age = 2022-birth_year;
+// document.write("Your age is : "+age+"<br>");
+// document.write("Your Birth year is :"+birth_year);
+
+
+// ASSIGMENT NO :14
+
+// var cus_name="azhar";
+//  var current_month="September";
+//  var num_unit="410";
+//  var charg_unit="16";
+//  var net_amount =charg_unit*num_unit;
+//  var let_amount=350;
+//  var gross_amount;
+
+//  document.write("Customer Name :"+cus_name+"<br>");
+//  document.write("Month : "+current_month+"<br>");
+//  document.write("Number of Unit  : "+num_unit+"<br>");
+//  document.write("Charges per unit : "+charg_unit+"<br>");
+//  document.write("Net Amount Payable (With Due Date): "+net_amount+"<br>");
+//  document.write("Late payment Charges : "+let_amount+"<br>");
+//  document.write("Gross Amount  Payable (after Due Date): "+(let_amount+net_amount)+"<br>");
+
+
+
+// CHAPTER 35 TO 38
+
+// // assigment no 1
+// function time(){
+
+//  var date= new Date()
+
+// document.write(date);
+
+// }
+// time();
+
+
+// // assigment no 2
+
+// function names(first_name, second_name){
+
+//  var first_name;
+//  var second_name;
+//  var full_name=first_name+" "+second_name;
+// document.write(full_name);
+// }
+// names("azhar", "ahmed");
+
+
+// assigment no 3
+
+// function add(a,b){
+
+// var a;
+// var b;
+// var c=a+b;
+//  return c;
+// }
+// var addition = add(+prompt("Enter 1st value"),+prompt("Enter 2nd value"))
+
+// alert("Addition is a+ b ="+addition);
+
+
+// assigment no 4
+// function add(a,b,operater){
+
+// var a;
+// var b;
+// if(operater=="+"){
+
+//   var c=  a+b;
+
+//   return c;
+
+// }
+// else if(operater=="-"){
+
+// var c=  a-b;
+
+// return c;
+
+// }
+// else if(operater=="*"){
+
+//     var c=  a*b;
+    
+//     return c;
+    
+//     }
+
+//     else if(operater=="/"){
+
+//         var c=  a/b;
+        
+//         return c;
+        
+//         }
+//         else if(operater=="%"){
+
+//             var c=  a%b;
+            
+//             return c;
+            
+//             }
+    
+
+// }
+// var addition = add(+prompt("Enter 1st value"),+prompt("Enter 2nd value"),prompt("Enter your operater"))
+
+// alert("Addition is a"+operater+"b ="+addition);
+
+
+// // assigment no 5
+// function square(num){
+
+// var num ;
+//  return add= num*num;
+
+
+// }
+
+// document.write(square(6))
+
+
+// // assigment no 7
+// function factorialize(num) {
+  
+//     var fact=1;
+
+//     if(num==0){
+
+//         alert("the Fatorial"+" "+ num +" "+"is 1")
+//     }
+//     else if(num<0){
+
+//         alert("the factorial negative number is not possible")
+
+//     }
+//     else {
+
+//         for(var i=1; i<=num; i++){
+
+//             fact = fact*i;
+
+//         }
+
+//         document.write(fact);
+
+//     }
+
+//   }
+
+
+// factorialize(+prompt("Enter  value"));
+
+
+// // assigment no 7
+
+// function count(first_num,last_num) {
+  
+
+//         for(var i=first_num; i<=last_num; i++){
+ 
+
+//             document.write(i+"<br>");
+
+//         }
+//   }
+
+// count(+prompt("Enter  starting number "),+prompt("Enter  ending number "));
+
+// // assigment no 8
+
+// function Hypotenus(base,perpendicular)
+//  { 
+
+//     function square(a){
+//         return a*a;
+//     }   
+//     return Math.sqrt(square(base) + square(perpendicular)) ; 
+
+
+// } 
+// var result = Hypotenus(+prompt("Enter Base"),+prompt("Enter perpendicular"))
+
+// document.write("Hypotenuse : "+result)
+
+// // assigment no 9
+
+// function area (length, width) {
+// return length * width;
+// }
+
+// var result= area(+prompt("Enter the lenght of your rectangle"),+prompt("Enter the width of your rectangle"));
+
+// alert(" The are of rectangle is : "+result);
+
+
+
+
+// // assigment no 10
+
+// var a=123;
+// // var a="madam";
+// function palindrom(){
+
+
+//     var rever= a.toString().split("").reverse().join("");
+
+//     var  string= a.toString();
+
+
+//     if(string == rever){
+
+//         document.write("this is palindrome ");
+//     }
+//     else{
+
+
+//         document.write("this is not palindrome")
+//     }
+
+// }
+
+// palindrom();
+
+// // assigment no 11
+
+// function uppercase(str)
+// {
+//   var array1 = str.split(' ');
+//   var newarray1 = [];
+    
+//   for(var i = 0; i < array1.length; i++){
+//       newarray1.push(array1[i].charAt(0).toUpperCase()+array1[i].slice(1));
+//   }
+//   return newarray1.join(' ');
+// }
+// alert( uppercase(prompt("Enter your string")));
+
+
+// // assigment no 12
+
+
+
+
+// // assigment no 13
+
+// function char_count(str, letter) 
+// {
+//  var letter_Count = 0;
+//  for (var position = 0; position < str.length; position++) 
+//  {
+//     if (str.charAt(position) == letter) 
+//       {
+//       letter_Count += 1;
+//       }
+//   }
+//   return letter_Count;
+// }
+
+// alert(char_count('JSResourceS.com', 'o'));
